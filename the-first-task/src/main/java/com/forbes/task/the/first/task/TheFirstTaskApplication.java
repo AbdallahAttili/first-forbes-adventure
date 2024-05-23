b65010001039bd9f6ef1aec9d21a08ed503c28c4
Abdallah-Attili <abdallah-attili@abdallah-attili-linux.scoiumtech.local>
2024-05-23 17:50:38 +0300

    starting the project from the spring initializr

---
+++ the-first-task/src/main/java/com/forbes/task/the/first/task/TheFirstTaskApplication.java
@@ -0,0 +1,13 @@
+package com.forbes.task.the.first.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFirstTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFirstTaskApplication.class, args);
+	}
+
+}
---
+++ the-first-task/src/test/java/com/forbes/task/the/first/task/TheFirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forbes.task.the.first.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFirstTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
